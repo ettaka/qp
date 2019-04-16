2e92c15d7241bc93f9324187f706b87d43239ba2
Eelis Takala <eelis.takala@gmail.com>
2019-04-16 14:16:15 +0200

    field quality: some fixes and add integrated values

--- field_quality.py
+++ field_quality.py
@@ -21,12 +21,18 @@ def read_value_table(table_name):
         rows = [line.split('\t') for line in f.read().decode('utf-8', 'ignore').replace('\r','').split('\n')]
 
     row_dicts = {}
+    last_non_empty_unit = 'units'
     for row in rows:
         if len(row)>3:
             row_dicts[row[0]]={}
             row_dict = row_dicts[row[0]]
             row_dict['name'] = row[0]
-            row_dict['unit'] = row[1]
+            if row[1] != '':
+                last_non_empty_unit = row[1]
+                row_dict['unit'] = row[1]
+            else:
+                row_dict['unit'] = last_non_empty_unit
+                
             try:
                 row_dict['values'] = np.array([float(value) for value in row[2:]])
             except:
@@ -52,11 +58,30 @@ def add_interp_to_row_dicts(data_dict):
         except:
             row_dict['interp'] = None
 
+def add_total_integral_to_row_dicts(data_dict):
+    pos_dict = data_dict['data rows']['pos']
+    for key in data_dict['data rows']:
+        integrand_dict = data_dict['data rows'][key]
+        try:
+            p0 = pos_dict['values'][0]
+            p1 = pos_dict['values'][-1]
+            integrand_dict['tot_integral'] = integrand_dict['interp'].integral(p0,p1) 
+        except:
+            integrand_dict['tot_integral'] = None
+
 def add_integrated_values(data_dicts):
+    pos_tot_integral = data_dicts['data rows']['pos']['tot_integral']
+    tot_len = data_dicts['data rows']['z']['values'][-1]-data_dicts['data rows']['z']['values'][0]
+    # The interpolation function is with respect to position number ['pos']:
+    # that is why we scale the total integral to that it is in terms of meters
+    scale = tot_len/pos_tot_integral  
+    print "tot_len", tot_len
+    print "pos_tot_integral", pos_tot_integral
+    print "integration scaling:", scale
     for key in data_dicts['data rows']:
         row_dict = data_dicts['data rows'][key]
         try:
-            row_dict['integrated values'] = np.array([row_dict['interp'].integral(1,i+1)/np.size(row_dict['values']) for i,tmp in enumerate(row_dict['values'])])
+            row_dict['integrated values'] = np.array([row_dict['interp'].integral(1,i+1)*scale for i,tmp in enumerate(row_dict['values'])])
         except:
             row_dict['integrated values'] = None
 
@@ -92,6 +117,7 @@ def add_centered_z(data_dict):
     z_centered_dict = {}
     data_dict['data rows']['z centered'] = z_centered_dict
     z_centered_dict['name'] = 'z centered'
+    z_centered_dict['unit'] = z_dict['unit']
     z_centered_dict['values'] = z_dict['values'] - z_dict['center']
     z_centered_dict['interp'] = scipy.interpolate.InterpolatedUnivariateSpline(data_dict['data rows']['pos']['values'], z_centered_dict['values'],k=1)
 
@@ -104,11 +130,12 @@ class RowPlotter:
         self.i=0
         self.args=args
         self.data_dict = data_dict
-        self.xdata = self.data_dict['data rows']['z centered']['values']
+        self.xdict = self.data_dict['data rows']['z centered']
+        self.xdata = self.xdict['values']
+        ax.set_xlabel(self.xdict['name'] + '(' + self.xdict['unit'] + ')')
 
     def plot(self, row, plot_integrated_values=False):
 
-        self.i+=1
         if plot_integrated_values: 
             ydata = row['integrated values'] 
             name = row['name'] + ' integ.'
@@ -119,18 +146,23 @@ class RowPlotter:
         save_fig = self.data_dict['casedir'] + '_' + name + '.png'
         ax.plot(self.xdata, ydata,'--'+self.colors[self.i]+self.markers[self.i],label=name)
 
-def plot_data(data_dict, args):
-    row_plotter = RowPlotter(data_dict, args)
-    row_plotter.plot(data_dict['data rows']['TF'])
-    row_plotter.plot(data_dict['data rows']['TF'], True)
+        ax.set_ylabel(row['name'] + '(' + row['unit'] + ')')
 
+        self.i+=1
+
+def plot_row(row_name, data_dict, args):
+    row_plotter = RowPlotter(data_dict, args)
+
+    save_fig = data_dict['casedir'] + '_' + row_name + '.png'
+
+    row_plotter.plot(data_dict['data rows'][row_name], args.plot_integrated_values)
+
+    print 'plotting', save_fig
     ax.legend(loc=args.legend_location)
     if args.show_plot:
         plt.show()
     else:
         plt.savefig(save_fig)
-
-    return
 
 def read_case(casedir, args):
     #meta not read
@@ -147,9 +179,14 @@ def read_case(casedir, args):
     add_interp_to_row_dicts(data_dict)
     find_center_position(data_dict)
     add_centered_z(data_dict)
+    add_total_integral_to_row_dicts(data_dict)
     add_integrated_values(data_dict)
+
+    row_names = ['b' + str(k) for k in range(3,16)]
     
-    plot_data(data_dict, args)
+    for row_name in row_names:
+        plt.cla()
+        plot_row(row_name, data_dict, args)
 
 
 if __name__ == '__main__':
@@ -157,6 +194,7 @@ if __name__ == '__main__':
     parser.add_argument('casedirs', nargs='+', type=str)
     parser.add_argument('-sp', '--show-plot', action='store_true', default=False) 
     parser.add_argument('-ll', '--legend-location', type=str, default='best')
+    parser.add_argument('-pi', '--plot-integrated-values', action='store_true', default=False)
 
     args = parser.parse_args()
     for casedir in args.casedirs:
