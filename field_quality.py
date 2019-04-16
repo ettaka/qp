4356f1264c9081297780a7839537d83dc7ea68b3
Eelis Takala <eelis.takala@gmail.com>
2019-04-16 11:04:56 +0200

    Add plot

--- field_quality.py
+++ field_quality.py
@@ -12,6 +12,10 @@ import scipy.optimize
 from matplotlib.collections import PatchCollection
 from matplotlib.patches import Rectangle
 
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.autoscale(enable=True, axis='y', tight=True)
+
 def read_value_table(table_name):
     with codecs.open(casedir + '/' + table_name) as f:
         rows = [line.split('\t') for line in f.read().decode('utf-8', 'ignore').replace('\r','').split('\n')]
@@ -82,9 +86,30 @@ def add_centered_z(main_field):
     z_centered_dict['values'] = z_dict['values'] - z_dict['center']
     z_centered_dict['interp'] = scipy.interpolate.InterpolatedUnivariateSpline(main_field['pos']['values'], z_centered_dict['values'],k=1)
 
+def plot_data(data_dict, args):
+    main_field = data_dict['main_field']
+    xdata = main_field['z centered']['values']
+    ydata = main_field['TF']['values']
+
+    #pos_dict = main_field['pos']
+    #TF_dict = main_field['TF']
+    #p0 = pos_dict['values'][0]
+    #ydata = [main_field['TF']['interp'].integral(p0, x) for x in pos_dict['values']]
+
+    save_fig = data_dict['casedir'] + '.png'
+
+    ax.plot(xdata, ydata,'-bo',label='Main field')
+
+    ax.legend(loc=args.legend_location)
+    if args.show_plot:
+        plt.show()
+    else:
+        plt.savefig(save_fig)
+
 def read_case(casedir, args):
     #meta not read
     data_dict = {}
+    data_dict['casedir'] = casedir
     main_field = read_value_table('main_field')
     normal_multipoles = read_value_table('normal_multipoles')
     skew_multipoles = read_value_table('skew_multipoles')
@@ -97,10 +122,15 @@ def read_case(casedir, args):
     add_interp_to_row_dicts(skew_multipoles, main_field['pos']['values'])
     find_center_position(main_field)
     add_centered_z(main_field)
+    
+    plot_data(data_dict, args)
+
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Plot transfer function')
     parser.add_argument('casedirs', nargs='+', type=str)
+    parser.add_argument('-sp', '--show-plot', action='store_true', default=False) 
+    parser.add_argument('-ll', '--legend-location', type=str, default='best')
 
     args = parser.parse_args()
     for casedir in args.casedirs:
