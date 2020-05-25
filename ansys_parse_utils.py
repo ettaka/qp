b68a9e3df7faf937a66af864b4bca7a3344c2584
Eelis Takala <eelis.tapani.takala@cern.ch>
2020-05-25 07:23:11 +0200

    parse ansys fixes

--- ansys_parse_utils.py
+++ ansys_parse_utils.py
@@ -1,39 +1,54 @@
 import os
 import pandas as pd
 import argparse
+from os import listdir
+from os.path import isfile, join
 
 def parse_ansys_2d_files(args):
+    """
+    possible octants: ['','_2','_3','_4','X','_2X','_3X','_4X']
+    """
     parsed_file_data_list = None
+    possible_octants = ['','_2','_3','_4','X','_2X','_3X','_4X']
     if args.ansys_2d_files is not None:
         parsed_file_data_list = []
         for path in args.ansys_2d_files:
-            if os.path.isdir(path): path += '/MQXF_2d_mech_results_output.txt'
-            name = '_'.join(path.split("QXF_2D")[1].split("-")[1].split("/")[0].split("_")[1:])
-            f = open(path, 'r')
-            file_dict=dict()
-            for line in f:
-                if 'Step' in line:
-                    colnames = line.split()
-                    for i, col_name in enumerate(colnames):
-                        file_dict[col_name]=[]
-                else:
-                    for i,value in enumerate(line.split()):
-                        colname = colnames[i]
-                        file_dict[colname].append(value)
-            parsed_file_data_list.append({
-                'DataFrame':pd.DataFrame(file_dict),
-                'name':name,
-                'path':path
-                })
-            df = parsed_file_data_list[-1]['DataFrame']
-            for col in list(df.keys()):
-                try:
-                    df[col] = pd.to_numeric(df[col])
-                except:
-                    pass
+            if os.path.isdir(path):
+                filebase = 'MQXF_2d_mech_results_output'
+                testfiles = [f for f in listdir(path) if isfile(join(path, f)) and filebase in f]
+                octantfiles = []
+                for octant in possible_octants:
+                    for testfile in testfiles:
+                        if str.lower(filebase+octant+'.txt') in testfile.lower():
+                            octantfiles.append((octant,testfile))
+                for octant,octantfile in octantfiles:
+                    octpath = path+'/' + octantfile
+                    print (octpath)
+                    name = '_'.join(octpath.split("QXF_2D")[1].split("-")[1].split("/")[0].split("_")[1:])+'_oct{}'.format(octant)
+                    f = open(octpath, 'r')
+                    file_dict=dict()
+                    for line in f:
+                        if 'Step' in line:
+                            colnames = line.split()
+                            for i, col_name in enumerate(colnames):
+                                file_dict[col_name]=[]
+                        else:
+                            for i,value in enumerate(line.split()):
+                                colname = colnames[i]
+                                file_dict[colname].append(value)
+                    parsed_file_data_list.append({
+                        'DataFrame':pd.DataFrame(file_dict),
+                        'name':name,
+                        'path':octpath
+                        })
+                    df = parsed_file_data_list[-1]['DataFrame']
+                    for col in list(df.keys()):
+                        try:
+                            df[col] = pd.to_numeric(df[col])
+                        except:
+                            pass
 
     return parsed_file_data_list
-
 
 def parse_ansys_3d_files(args):
     parsed_file_data_list = []
